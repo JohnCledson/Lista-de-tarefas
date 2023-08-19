d67a3729a31b69cd981b38d3b1d7597ba34f7415
John Cledson <cledsonjohn123@gmail.com>
2023-08-18 22:16:30 -0300

    Update

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
-$('form').on('submit', function(e) {
-  e.preventDefault()
-  const tarefa = $('#tarefa')
-  const novoItem = $('.tarefas')
-  $(`<li>${tarefa.val()}</li>`).appendTo(novoItem)
-  console.log('t')
+let quantidadeTarefas = 0
+$(document).ready(function() {
+  $('form').on('submit', function(e) {
+    e.preventDefault()
+    const tarefa = $('#tarefa')
+    const novoItem = $('.tarefas')
+    $(`<li><a>${tarefa.val()}</a></li>`).appendTo(novoItem)
+    console.log('t')
+  })
+  
+  $('.tarefas').on('click', 'li', function() {
+    $(this).toggleClass("tarefaRealizada")
+  })
 })
-
-$(".tarefas").on("click", "li", function() {
-  $(this).toggleClass("tarefaRealizada");
-});
